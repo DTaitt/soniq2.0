8a88fe977f3bed4b57c5177971c83329a71d49dc
Dtaitt <Donovan.taitt@yahoo.com>
2018-02-24 02:51:37 -0500

    added links to menu

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,5 +1,6 @@
 //@flow 
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 type Props = {};
 type State = {};
@@ -10,9 +11,21 @@ export default class Sidebar extends Component<Props, State> {
             <aside className="menu">
                 <ul>
                     <li>Library</li>
-                    <li>Albums</li>
-                    <li>Artists</li>
-                    <li>Songs</li>
+                    <li>
+                        <Link to='/artists' >
+                            Artists
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to='/albums' >
+                            Albums
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to='/tracks' >
+                            Tracks
+                        </Link>
+                    </li>
                 </ul>
             </aside>
         )
